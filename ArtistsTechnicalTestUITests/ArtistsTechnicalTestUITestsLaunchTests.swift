6eb67324fbc459d82155bbda87a10f055582abeb
Jeremy  Bailly <jeremy.bailly@hotmail.com>
2024-10-18 18:17:57 +0200

    first commit

---
+++ ArtistsTechnicalTestUITests/ArtistsTechnicalTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArtistsTechnicalTestUITestsLaunchTests.swift
+//  ArtistsTechnicalTestUITests
+//
+//  Created by Jeremy  Bailly  on 18/10/2024.
+//
+
+import XCTest
+
+final class ArtistsTechnicalTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
