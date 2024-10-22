9c0d26997164ab28cf49f4c0dcdb49310dabfb2e
Jeremy  Bailly <jeremy.bailly@hotmail.com>
2024-10-22 09:49:20 +0200

    Comment DTOs

--- ArtistsTechnicalTest/Features/Search/Data/DTOs/ArtistDTO.swift
+++ ArtistsTechnicalTest/Features/Search/Data/DTOs/ArtistDTO.swift
@@ -5,6 +5,8 @@
 //  Created by Jeremy  Bailly  on 19/10/2024.
 //
 
+/// A struct representing an artist.
+/// Conforms to the `Decodable`protocol to enable decoding from JSON data.
 struct ArtistDTO: Decodable {
     let id: Int
     let title: String
--- ArtistsTechnicalTest/Features/Search/Data/DTOs/SearchArtistsResultDTO.swift
+++ ArtistsTechnicalTest/Features/Search/Data/DTOs/SearchArtistsResultDTO.swift
@@ -5,6 +5,8 @@
 //  Created by Jeremy  Bailly  on 19/10/2024.
 //
 
+/// A struct representing the result of a search for artists.
+/// Conforms to the `Decodable`protocol to enable decoding from JSON data.
 struct SearchArtistsResultDTO: Decodable {
     let data: [ArtistDTO]
 }
